a90e53df492c6017a2116ba8e74fa8c418511faa
YudinYury <yudin.yury@gmail.com>
2017-06-23 19:29:02 +0300

    String tasks (by HackerRank) are solved

--- Whats_Your_Name.py
+++ Whats_Your_Name.py
@@ -1,5 +1,5 @@
 ''' Python question by HackerRank
-What's Your Name
+TODO 1: What's Your Name
 You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
 Hello firstname lastname! You just delved into python.
 Input Format: The first line contains the first name, and the second line contains the last name.
@@ -12,13 +12,135 @@ Sample Output
 Hello Guido Rossum! You just delved into python.
 Explanation
 The input read by the program is stored as a string data type. A string is a collection of characters.
+TODO 2: Mutations
+We have seen that lists are mutable (they can be changed), and tuples are immutable (they cannot be changed).
+Let's try to understand this with an example.
+You are given an immutable string, and you want to make changes to it.
+Example
+string = "abracadabra"
+You can access an index by:
+print string[5]
+a
+What if you would like to assign a value?
+string[5] = 'k'
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: 'str' object does not support item assignment
+How would you approach this?
+One solution is to convert the string to a list and then change the value.
+Example
+string = "abracadabra"
+l = list(string)
+l[5] = 'k'
+string = ''.join(l)
+print string
+abrackdabra
+Another approach is to slice the string and join it back.
+Example
+string = string[:5] + "k" + string[6:]
+print string
+abrackdabra
+Task
+Read a given string, change the character at a given index and then print the modified string.
+Input Format
+The first line contains a string, .
+The next line contains an integer , denoting the index location and a character  separated by a space.
+Output Format
+Using any of the methods explained above, replace the character at index  with character .
+TODO 3: Find a string
+In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
+NOTE: String letters are case-sensitive.
+Input Format
+The first line of input contains the original string. The next line contains the substring.
+Constraints
+1 <= len(string) <=200
+Each character in the string is an ascii character.
+Output Format
+Output the integer number indicating the total number of occurrences of the substring in the original string.
+Sample Input
+ABCDCDC
+CDC
+Sample Output
+2
+Concept
+Some string processing examples, such as these, might be useful.
+There are a couple of new concepts:
+In Python, the length of a string is found by the function len(s), where  is the string.
+To traverse through the length of a string, use a for loop:
+for i in range(0, len(s)):
+    print (s[i])
+A range function is used to loop over some length:
+range (0, 5)
+Here, the range loops over 0 to 4.  5 is excluded.
+TODO 4:
+You are given a string .
+Your task is to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+Input Format
+A single line containing a string S.
+Constraints 0 < len(S) < 1000
+Output Format
+In the first line, print True if S has any alphanumeric characters. Otherwise, print False.
+In the second line, print True if S has any alphabetical characters. Otherwise, print False.
+In the third line, print True if S has any digits. Otherwise, print False.
+In the fourth line, print True if S has any lowercase characters. Otherwise, print False.
+In the fifth line, print True if S has any uppercase characters. Otherwise, print False.
+Sample Input
+qA2
+Sample Output
+True
+True
+True
+True
+True
 '''
 
+from functools import *
+
 #############################################################################################################
+#TODO 1:
 def print_full_name(a, b):
     print("Hello {} {}! You just delved into python.".format(a,b))
 #############################################################################################################
-def main():
+#TODO 2:
+def mutate_string(string, position, character):
+    return string[:position] + character + string[position+1:]
 
 #############################################################################################################
-main()
+#TODO 3:
+def count_substring(string, sub_string):
+    count=0
+    start=0
+    while True:
+        n=string.find(sub_string, start)
+        if n == -1:
+            break
+        else:
+            count+=1
+            start=n+1
+    return count
+#############################################################################################################
+#TODO 4:
+s='qA2'
+
+m=list(map(str.isalnum ,[i for i in s]))
+print(reduce(lambda x,y: x or y,m))
+
+m=list(map(str.isalpha ,[i for i in s]))
+print(reduce(lambda x,y: x or y,m))
+
+m=list(map(str.isdigit ,[i for i in s]))
+print(reduce(lambda x,y: x or y,m))
+
+m=list(map(str.islower ,[i for i in s]))
+print(reduce(lambda x,y: x or y,m))
+
+m=list(map(str.isupper ,[i for i in s]))
+print(reduce(lambda x,y: x or y,m))
+
+#############################################################################################################
+
+# def main():
+
+
+#############################################################################################################
+# main()
