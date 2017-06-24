c79ec1cb17b37ed9149cae134ed68403ee26d8b9
YudinYury <yudin.yury@gmail.com>
2017-06-24 20:14:46 +0300

    Text Alignment Question by HackerRank solved

--- Whats_Your_Name.py
+++ Whats_Your_Name.py
@@ -92,6 +92,15 @@ True
 True
 True
 True
+TODO 5:
+You are given a partial code that is used for generating the HackerRank Logo of variable thickness.
+Your task is to replace the blank (______) with rjust, ljust or center.
+Input Format
+A single line containing the thickness value for the logo.
+Constraints
+The thickness must be an odd number.
+0 <= thickness <= 50
+Output Format:  Output the desired logo.
 '''
 
 from functools import *
@@ -121,21 +130,24 @@ def count_substring(string, sub_string):
 #############################################################################################################
 #TODO 4:
 s='qA2'
+#
+# m=list(map(str.isalnum ,[i for i in s]))
+# print(reduce(lambda x,y: x or y,m))
+#
+# m=list(map(str.isalpha ,[i for i in s]))
+# print(reduce(lambda x,y: x or y,m))
+#
+# m=list(map(str.isdigit ,[i for i in s]))
+# print(reduce(lambda x,y: x or y,m))
+#
+# m=list(map(str.islower ,[i for i in s]))
+# print(reduce(lambda x,y: x or y,m))
+#
+# m=list(map(str.isupper ,[i for i in s]))
+# print(reduce(lambda x,y: x or y,m))
 
-m=list(map(str.isalnum ,[i for i in s]))
-print(reduce(lambda x,y: x or y,m))
-
-m=list(map(str.isalpha ,[i for i in s]))
-print(reduce(lambda x,y: x or y,m))
-
-m=list(map(str.isdigit ,[i for i in s]))
-print(reduce(lambda x,y: x or y,m))
-
-m=list(map(str.islower ,[i for i in s]))
-print(reduce(lambda x,y: x or y,m))
-
-m=list(map(str.isupper ,[i for i in s]))
-print(reduce(lambda x,y: x or y,m))
+#############################################################################################################
+#TODO 5:
 
 #############################################################################################################
 
@@ -144,3 +156,28 @@ print(reduce(lambda x,y: x or y,m))
 
 #############################################################################################################
 # main()
+
+thickness = int(input()) #This must be an odd number
+# thickness = 5
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
