77f91f79e65b05bc0cce17514ac4bad613738e62
YudinYury <yudin.yury@gmail.com>
2017-06-28 17:12:48 +0300

    HackerRank task "String Formatting" have done

--- Text_Wrap.py
+++ Text_Wrap.py
@@ -21,11 +21,24 @@ String Split and Join
 You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
 Input Format: The first line contains a string consisting of space separated words.
 Output Format: Print the formatted string as explained above.
-TODO 2:
+TODO 4:
 Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
 Mat size must be N x M. (N is an odd natural number, and M is 3 times N.)
 The design should have 'WELCOME' written in the center.
 The design pattern should only use |, . and - characters.
+
+Given an integer, N, print the following values for each integer  from  to :
+Decimal
+Octal
+Hexadecimal (capitalized)
+Binary
+The four values must be printed on a single line in the order specified above for each  from  to . Each value should be
+space-padded to match the width of the binary value of .
+Input Format: A single integer denoting N.
+Constraints: 1 <= N <= 99
+Output Format: Print N lines where each line  (in the range 1 <= i <= N) contains the respective decimal, octal,
+capitalized hexadecimal, and binary values of i. Each printed value must be formatted to the width of the binary value of n.
+
 '''
 
 import textwrap
@@ -58,16 +71,53 @@ def split_and_join(line):
 
 #############################################################################################################
 #TODO 4:
+# def main():
+#     N = 7
+#     M = 21
+#     for i in range(1, N, 2):
+#         print(('.|.'*i).center(M,'-'))
+#     print(('WELCOME').center(M,'-'))
+#     for i in range(N - 2, -1, -2):
+#         print(('.|.'*i).center(M,'-'))
+
+#############################################################################################################
+#TODO 5: String Formatting
+def print_formatted(number):
+    # your code goes here
+    num_s = bin(number)[2:]
+    width = len(num_s)
+    for it_number in range(1,number+1):
+        num_s = ''
+        num_s = str(it_number).rjust(width) + ' ' + oct(it_number)[2:].rjust(width) + ' ' + hex(it_number)[2:].upper().rjust(width) + ' ' + bin(it_number)[2:].rjust(width)
+        print(num_s)
+    num_s = num_s.rjust(width)
+    return num_s
+
+#############################################################################################################
+#TODO 6: Alphabet Rangoli
+def str_rangoli(string, new_size):
+    if len(string) == 1:
+        return string
+    else:
+        return string[0] + '-' + str_rangoli(string[1:],new_size-1)+ '-' + string[0]
+
+def print_rangoli(size):
+    # your code goes here
+    alph_str = string.ascii_lowercase
+    num_char = size+1
+    width = size*2-1
+    str  = str_rangoli(alph_str,size)
+    # for i,ch in enumerate(alph_str[num_char::-1]):
+    #     s =
+    #     print(s.center(width,'-'))
+
+    print(str.center(width,'-'))
+
+
 
 #############################################################################################################
 def main():
-    N = 7
-    M = 21
-    for i in range(1, N, 2):
-        print(('.|.'*i).center(M,'-'))
-    print(('WELCOME').center(M,'-'))
-    for i in range(N - 2, -1, -2):
-        print(('.|.'*i).center(M,'-'))
+    print_formatted(3)
 
 #############################################################################################################
 if __name__ == '__main__':
