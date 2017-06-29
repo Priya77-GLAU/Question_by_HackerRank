6b027ba72d6b56129278a1c71b26d75ede7bed72
YudinYury <yudin.yury@gmail.com>
2017-06-29 12:37:21 +0300

    Task "Capitalize!" have done

--- Text_Wrap.py
+++ Text_Wrap.py
@@ -26,7 +26,7 @@ Mr. Vincent works in a door mat manufacturing company. One day, he designed a ne
 Mat size must be N x M. (N is an odd natural number, and M is 3 times N.)
 The design should have 'WELCOME' written in the center.
 The design pattern should only use |, . and - characters.
-
+TODO 5:
 Given an integer, N, print the following values for each integer  from  to :
 Decimal
 Octal
@@ -38,7 +38,22 @@ Input Format: A single integer denoting N.
 Constraints: 1 <= N <= 99
 Output Format: Print N lines where each line  (in the range 1 <= i <= N) contains the respective decimal, octal,
 capitalized hexadecimal, and binary values of i. Each printed value must be formatted to the width of the binary value of n.
-
+TODO 6:
+You are given an integer, N. Your task is to print an alphabet rangoli of size N. (Rangoli is a form of Indian folk art based on creation of patterns.)
+Different sizes of alphabet rangoli are shown below:
+#size 3
+----c----
+--c-b-c--
+c-b-a-b-c
+--c-b-c--
+----c----
+TODO 7:
+You are given a string S. Your task is to capitalize each word of S.
+Input Format: A single line of input containing the string, S.
+Constraints:  0 < len(s) < 1000
+The string consists of alphanumeric characters and spaces.
+Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
+Output Format: Print the capitalized string, S.
 '''
 
 import textwrap
@@ -126,11 +141,28 @@ def print_rangoli_new(size):
         if i == (size):
             k = -k
 
-
+# TODO 7: "Capitalize!"
+def capitalize(string):
+    new_string = ''
+    i = 0
+    for s in string:
+        if s == ' ':
+            new_string = new_string + s
+            i = 0
+        elif i == 0:
+            new_string = new_string + s.upper()
+            i += 1
+        else:
+            new_string = new_string + s
+            i += 1
+    return new_string
 #############################################################################################################
 def main():
     # print_rangoli(5)
-    print_rangoli_new(5)
+    # print_rangoli_new(5)
+    test_string = 'hello woRld from my 100s hearts'
+    # test_string = 'q w e r t y u i o p a s d f g h j  k l z x c v b n m Q W E R T Y U I O P A S D F G H J  K L Z X C V B N M'
+    print(capitalize(test_string))
 
 #############################################################################################################
 if __name__ == '__main__':
