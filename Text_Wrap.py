ac8423ad2634a4e79cd30ea2db90be299a18fad0
YudinYury <yudin.yury@gmail.com>
2017-07-01 14:14:14 +0300

    lesson 2.4 homework_12 have done

--- Text_Wrap.py
+++ Text_Wrap.py
@@ -54,6 +54,29 @@ Constraints:  0 < len(s) < 1000
 The string consists of alphanumeric characters and spaces.
 Note: in a word only the first character is capitalized. Example 12abc when capitalized remains 12abc.
 Output Format: Print the capitalized string, S.
+TODO 8:
+Kevin and Stuart want to play the 'The Minion Game'.
+Game Rules
+Both players are given the same string, S.
+Both players have to make substrings using the letters of the string S.
+Stuart has to make words starting with consonants.
+Kevin has to make words starting with vowels.
+The game ends when both players have made all possible substrings.
+
+Scoring: A player gets +1 point for each occurrence of the substring in the string .
+
+For Example:  String  = BANANA
+Kevin's vowel beginning word = ANA
+Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
+
+Your task is to determine the winner of the game and their score.
+
+Input Format:
+A single line of input containing the string S.
+Note: The string S will contain only uppercase letters: [A-Z].
+Constraints: 0 < len(s) <= 10**6
+Output Format: Print one line: the name of the winner and their score separated by a space.
+If the game is a draw, print Draw.
 '''
 
 import textwrap
@@ -156,13 +179,138 @@ def capitalize(string):
             new_string = new_string + s
             i += 1
     return new_string
+
+#############################################################################################################
+# TODO 8: The Minion Game
+# Task "The Minion Game" have done
+# Stuart = consonants = согласные
+# Kevin = vowels = гласные
+# inpuy
+#
+
+def char_is_consonants(char_str):
+    consonants_str = 'BCDFGHJKLMNPQRSTVWXZ'
+    if char_str in consonants_str:
+        return True
+    else:
+        return False
+
+
+def char_is_vowels(char_str):
+    vowels_str = 'AEIOUY'
+    if char_str in vowels_str:
+        return True
+    else:
+        return False
+
+
+def scoring(arg_str, arg_sub_str):
+    score_int = 0
+    index = arg_str.find(arg_sub_str)
+    while index != -1:
+        score_int += 1
+        # print('arg_sub_str   =', arg_sub_str, arg_str[index:], score_int)
+        index = arg_str.find(arg_sub_str, index+1)
+    return score_int
+
+
+def find_chars(arg_str, arg_sub_str):
+    substrings_list = []
+    for ss in arg_sub_str:
+        if ss in arg_str:
+            substrings_list.append(ss)
+    return substrings_list
+
+def unique_list(arg_list):
+    result_list = []
+    for i in arg_list:
+        if i not in result_list:
+            result_list.append(i)
+    return result_list
+
+
+
+
+#############################################################################################################
+def find_consonant_substrings(str_arg):
+    substrings_list = []
+    index_start = 0
+    index_end = None
+    str_len = len(str_arg)
+    while index_start < str_len:
+        if char_is_consonants(str_arg[index_start]):
+            for index_end in range(len(str_arg[index_start:])):
+                substrings_list.append(str_arg[index_start:index_start+index_end+1])
+            index_start += 1
+        else:
+            index_start += 1
+            continue
+    return substrings_list
+
+
+def find_vowel_substrings(str_arg):
+    substrings_list = []
+    index_start = 0
+    index_end = None
+    str_len = len(str_arg)
+    while index_start < str_len:
+        if char_is_vowels(str_arg[index_start]):
+            for index_end in range(len(str_arg[index_start:])):
+                substrings_list.append(str_arg[index_start:index_start+index_end+1])
+            index_start += 1
+        else:
+            index_start += 1
+            continue
+    return substrings_list
+
+
+# print Name of winner, his score
+def minion_game(string):
+    # your code goes here
+    kevin_vowels_score = 0
+    stuart_consonants_score = 0
+    stuart_list = set(find_consonant_substrings(string))
+    for str_from_list in stuart_list:
+        stuart_consonants_score += scoring(string, str_from_list)
+
+    # kevin_list = set(find_vowel_substrings(string))
+    kevin_l = find_vowel_substrings(string)
+    # kevin_list = kevin_l
+    # kevin_list = list(set(kevin_l))
+    kevin_list = unique_list(kevin_l)
+
+    # kevin_collect = set()
+    # new_list = [e for e in kevin_l if e not in kevin_collect and not kevin_collect.add(e)]
+
+    # print('kevin_l list len =', len(kevin_l), 'kevin_l set len =', len(kevin_list))
+    # print(kevin_list)
+    for str_from_list in kevin_list:
+        # print('str_from_list =', str_from_list)
+        kevin_vowels_score += scoring(string, str_from_list)
+
+    if stuart_consonants_score > kevin_vowels_score:
+        print('Stuart', stuart_consonants_score)
+    elif kevin_vowels_score > stuart_consonants_score:
+        print('Kevin', kevin_vowels_score)
+    else:
+        print('Draw')
+
+
+
 #############################################################################################################
 def main():
     # print_rangoli(5)
     # print_rangoli_new(5)
-    test_string = 'hello woRld from my 100s hearts'
+    # test_string = 'hello woRld from my 100s hearts'
     # test_string = 'q w e r t y u i o p a s d f g h j  k l z x c v b n m Q W E R T Y U I O P A S D F G H J  K L Z X C V B N M'
-    print(capitalize(test_string))
+    test_input_str = 'EQQAEAOQYEQEYYOEEQQYAOEEAQEEOOEYAYOEYAYAEOQYAAYAOYYOQAAYEQAOOAQEAEYAOEEQYYEEAOAOAEQOEYOAOEYOOAAOQ' \
+                     'EOYEAYYOEAOAQEYYEOQEEEYAOOAYOOAQAEOYOYAEOYQEEEOOQOEAOAAQAOQEYOQEAEAEOOOOQOYQOEQQYEEEYEEOQYYYOEQOQ' \
+                     'EYAYQQOYEEOAEQOEEEEAAEEYAAQAAQAAYOEAQAQYEYYYEAOYOQEQOOEQOYAYAEEYQAYYQYYAEAYOEYEAOQQQOYYYOYYOYYQYA' \
+                     'OEOAOAOYEAAOEOEAEYQAEAQOEOYEEAQOAOQEYOEQOAQQEEYOOAQQOOEYQAQOEEOOOAAQOQEYYOEOOQOOAEYEOOAEQYQOAEYYYA' \
+                     'QAYOEYOEYYEEOEEOAYAEEQEQOAAAYAEYQQAYOYQQOAEAOQOOYAEEOAEQAQEEQYOOEEAEEAAOYQYQAOEQYOYEQEAAOYAQAQYEAQEQEEOQQQYEYOQ'
+    test_output_str = 'Kevin 82011'
+    games_str = 'BANANA'
+    aa = minion_game(test_input_str)
 
 #############################################################################################################
 if __name__ == '__main__':
