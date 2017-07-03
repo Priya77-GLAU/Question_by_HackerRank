30bbefeee6881a1bc91358336a3a56196e981c12
YudinYury <yudin.yury@gmail.com>
2017-07-03 16:33:10 +0300

    "Min and Max" (Numpy) by HackerRank have done (use special func for input)

--- Numpy_Challenges .py
+++ Numpy_Challenges .py
@@ -7,13 +7,20 @@ The first line of input contains space separated values of N and M.
 The next N lines contains M space separated integers.
 Output Format: Compute the sum along axis 0. Then, print the product of that sum.
 
+TODO 2:
+You are given a 2-D array with dimensions NxM.
+Your task is to perform the min function over axis 1 and then find the max of that.
+Input Format:
+The first line of input contains the space separated values of N and M.
+The next N lines contains M space separated integers.
+Output Format: Compute the min along axis 1 and then print the max of that result.
+
 '''
 
 import numpy
 import sys
 
 
-#TODO 1:
 def get_array_from_input():
     array_size = tuple([int(item) for item in input().split()])
     array_list = []
@@ -21,26 +28,40 @@ def get_array_from_input():
         array_list.append(tuple([int(item) for item in input().split()]))
 
     new_array = numpy.array(array_list)
-
     return new_array
 
-# array_size = tuple([int(item) for item in input().split()])
-# array_list = []
-# for i in range(array_size[0]):
-#     array_list.append(tuple([int(item) for item in input().split()]))
-
-# my_array = numpy.array(array_list)
-my_array = get_array_from_input()
-step_1 = numpy.sum(my_array, axis = 0)
-print (step_1)         #Output : [4 6]
-print(numpy.prod(step_1))
 
 
+#TODO 1: "Sum and Prod" (Numpy)
+def sum_and_prod():
+    # array_size = tuple([int(item) for item in input().split()])
+    # array_list = []
+    # for i in range(array_size[0]):
+    #     array_list.append(tuple([int(item) for item in input().split()]))
+
+    # my_array = numpy.array(array_list)
+    my_array = get_array_from_input()
+    step_1 = numpy.sum(my_array, axis = 0)
+    print (step_1)
+    print(numpy.prod(step_1))
+    return 0
 
 
-# def main():
+#TODO 2: "Min and Max" (Numpy)
+def min_and_max():
+    my_array = get_array_from_input()
+    step_1 = numpy.min(my_array, axis=1)
+    print(numpy.max(step_1))
+    return 0
 
 
 
-# if __name__ == '__main__':
-#     main()
+def main():
+    # sum_and_prod()
+    min_and_max()
+
+
+
+
+if __name__ == '__main__':
+    main()
