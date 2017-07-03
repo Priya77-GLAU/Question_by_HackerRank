9048c7a69ef23807f0e46aceacb0b9fa274490ef
YudinYury <yudin.yury@gmail.com>
2017-07-03 17:46:25 +0300

    update "Mean, Var, and Std" (Numpy)

--- Numpy_Challenges .py
+++ Numpy_Challenges .py
@@ -16,7 +16,22 @@ The next N lines contains M space separated integers.
 Output Format: Compute the min along axis 1 and then print the max of that result.
 
 TODO 3:
+You are given a 2-D array of size NxM.
+Your task is to find:
+The mean along axis 1
+The var along axis 0
+The std along axis None
 
+Input Format:
+The first line contains the space separated values of N and M.
+The next N lines contains M space separated integers.
+
+Output Format
+First, print the mean.
+Second, print the var.
+Third, print the std.
+
+TODO 4:
 '''
 
 import numpy
