5f642922092afc1cc91608435994a0be01198ab9
YudinYury <yudin.yury@gmail.com>
2017-07-06 16:50:10 +0300

    "Set .add()" is solved "Set .discard(), .remove() & .pop()"  is solved

--- Sets_Challenges.py
+++ Sets_Challenges.py
@@ -31,6 +31,30 @@ The second line contains n integers, the elements of the array.
 The third and fourth lines contain m integers, A and B, respectively.
 Output Format: Output a single integer, your total happiness.
 
+TODO 4: Set .add()
+Apply your knowledge of the .add() operation to help your friend Rupal.
+Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection. She asked for your help. You pick the stamps one by one from a stack of  country stamps.
+Find the total number of distinct country stamps.
+Input Format:
+The first line contains an integer N, the total number of country stamps.
+The next N lines contains the name of the country where the stamp is from.
+Constraints: 0 < N < 1000
+Output Format:  Output the total number of distinct country stamps on a single line.
+Explanation:
+UK and France repeat twice. Hence, the total number of distinct country stamps is 5 (five).
+
+TODO 4: "Set .discard(), .remove() & .pop()"
+You have a non-empty set S, and you have to execute N commands given in N lines.
+The commands will be pop, remove and discard.
+
+Input Format:
+The first line contains integer N, the number of elements in the set S.
+The second line contains n space separated elements of set s. All of the elements are non-negative integers, less than or equal to 9.
+The third line contains integer N, the number of commands.
+The next N lines contains either pop, remove and/or discard commands followed by their associated value.
+
+Constraints 0 < n < 20,  0 < N < 20
+Output Format: Print the sum of the elements of set S on a single line.
 '''
 
 import numpy
@@ -73,31 +97,75 @@ def symmetric_difference():
 
 
 #TODO 3: "No Idea"
-array_size = tuple([int(item) for item in input().split()])
-array_list = []
-array_list = [int(i) for i in input().split(' ')]
-print(array_list)
-A_set = set([int(i) for i in input().split(' ')])
-B_set = set([int(i) for i in input().split(' ')])
-print(A_set)
-print(B_set)
-my_happiness = 0
-for i in array_list:
-    if i in A_set:
-        my_happiness += 1
-    elif i in B_set:
-        my_happiness -= 1
-print(my_happiness)
-
-# def main():
-    # sum_and_prod()
-    # min_and_max()
-    # mean_var_std()
-    # dot_cross()
-    # print(my_array)
+def no_idea():
+    array_size = tuple([int(item) for item in input().split()])
+    array_list = []
+    array_list = [int(i) for i in input().split(' ')]
+    print(array_list)
+    A_set = set([int(i) for i in input().split(' ')])
+    B_set = set([int(i) for i in input().split(' ')])
+    print(A_set)
+    print(B_set)
+    my_happiness = 0
+    for i in array_list:
+        if i in A_set:
+            my_happiness += 1
+        elif i in B_set:
+            my_happiness -= 1
+    print(my_happiness)
+    return my_happiness
 
 
+#TODO 4: "Set .add()"
+def set_add():
+    countries = set()
+    len_of_list = int(input())
+    for i in range(len_of_list):
+        countries.add(input().lstrip().rstrip())
+    print(len(countries))
 
 
-# if __name__ == '__main__':
-#     main()
+#TODO 5: "Set .discard(), .remove() & .pop()"
+def get_command():
+    digits = 0
+    command_str = ''
+    input_list = input().lstrip().rstrip().split(' ')
+    command_str = input_list[0]
+    if len(input_list) == 2:
+        digits = int(input_list[1])
+
+    return command_str, digits
+
+
+def discard_remove_pop():
+    n = int(input())
+    s = set(map(int, input().split()))
+
+num_of_command = int(input())
+for i in range(num_of_command):
+    new_command, digit = get_command()
+    if new_command == 'pop':
+        s.pop()
+    elif new_command == 'remove':
+        s.remove(digit)
+    elif new_command == 'discard':
+        # print('command = {0}({1})'.format(new_command, digit))
+        s.discard(digit)
+
+summary_set = 0
+while s:
+    summary_set += s.pop()
+
+print(summary_set)
+
+#TODO 6:
+
+
+def main():
+    # set_add()
+    discard_remove_pop()
+
+
+
+if __name__ == '__main__':
+    main()
