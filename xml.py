234c7ea5020f187991fd98722c5009d5142654d0
YudinYury <yudin.yury@gmail.com>
2017-07-29 13:32:33 +0300

    solved all the XML Hackerrank challenges in Python!

--- xml.py
+++ xml.py
@@ -13,16 +13,20 @@ The link tag has three attributes - rel, type and href.
 So, the total score is 1+1+3=5.
 There may be any level of nesting in the XML document. To learn about XML parsing, refer here.
 
+TODO 2: "XML2 - Find the Maximum Depth"
+You are given a valid XML document, and you have to print the maximum level of nesting in it. Take the depth of the root as 0.
+Input Format:
+The first line contains N, the number of lines in the XML document.
+The next N lines follow containing the XML document.
+Output Format:
+Output a single line, the integer value of the maximum level of nesting in the XML document.
+
 '''
 
 
 # import lxml
 # import lxml.html as html
-# from lxml import etree as etree
-
-
 # import xml.etree.cElementTree as etree
-# from xml.etree import ElementTree as etree
 
 
 # TODO 1: "XML 1 - Find the Score"
@@ -40,15 +44,10 @@ def xml_1_find_the_score(node):
     # number_of_line = int(input().lstrip().rstrip())
     # for _ in range(number_of_line):
     #     xml_line += input() + '\n'
-    # print(xml_line)
-    # root = obj.fromstring(xml_line)
-    # attrib = root.attrib
-    # print(attrib)
     # root = etree.fromstring(xml_line)
     # root = obj.fromstring(xml_line)
     # attrib = root.attrib
     # number_of_attributes += len(attrib)
-    #
     # for appt in root.getchildren():
     #     number_of_attributes += len(appt.attrib)
     #     # if len(appt.attrib):
@@ -58,11 +57,24 @@ def xml_1_find_the_score(node):
     # print(number_of_attributes)
 
 
+# TODO 2: "XML2 - Find the Maximum Depth"
+maxdepth = 0
+
+
+def depth(elem, level):
+    global maxdepth
+    # your code goes here
+    level += 1
+    if level > maxdepth:
+        maxdepth = level
+    for new_elem in elem.findall('.//'):
+        depth(new_elem, level)
+    return 0
+
 def main():
 
 
 # xml_1_find_the_score()
-
 
 
 if __name__ == '__main__':
