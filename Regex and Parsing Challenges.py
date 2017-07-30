aedf1e795f16c91f500aaf82fdca280ba51f7961
YudinYury <yudin.yury@gmail.com>
2017-07-30 15:26:00 +0300

    "Re.findall() & Re.finditer()" is solved

--- Regex and Parsing Challenges.py
+++ Regex and Parsing Challenges.py
@@ -38,12 +38,23 @@ Input Format: A single line of input containing the string S.
 Output Format: Print the numbers obtained after splitting the string on separate lines.
 
 TODO 3: "Group(), Groups() & Groupdict()"
-
 You are given a string S.
 Your task is to find the first occurrence of an alphanumeric character in  S(read from left to right) that has consecutive repetitions.
 Input Format: A single line of input containing the string S.
 Constraints: 0 < len(S) < 100
 Output Format: Print the first occurrence of the repeating character. If there are no repeating characters, print -1.
+
+TODO 4: "Re.findall() & Re.finditer()"
+You are given a string S. It consists of alphanumeric characters, spaces and symbols(+,-).
+Your task is to find all the substrings of S that contains 2 or more vowels.
+Also, these substrings must lie in between 2 consonants and should contain vowels only.
+Note :
+Vowels are defined as: AEIOU and aeiou.
+Consonants are defined as: QWRTYPSDFGHJKLZXCVBNM and qwrtypsdfghjklzxcvbnm.
+Input Format: A single line of input containing string S.
+Constraints: 0 < len(S) < 100
+Output Format: Print the matched substrings in their order of occurrence on separate lines.
+If no match is found, print -1.
 
 '''
 
@@ -102,13 +113,35 @@ def introduction_to_regex():
 
 
 # TODO 3: "Group(), Groups() & Groupdict()"
+# source_s = input().lstrip().rstrip()
+# pattern = re.compile(r'\w+')
+# s_list = re.search('[\w]+', source_s)
+# result = list(filter(lambda x: re.search(x, source_s), [s * 2 for s in source_s if s.isalnum()]))
+# if len(result):
+#     print(result[0][0])
+# else:
+#     print(-1)
+
+
+# TODO 4: "Re.findall() & Re.finditer()"
 source_s = input().lstrip().rstrip()
-pattern = re.compile(r'\w+')
-s_list = re.search('[\w]+', source_s)
-result = list(filter(lambda x: re.search(x, source_s), [s * 2 for s in source_s if s.isalnum()]))
-if len(result):
-    print(result[0][0])
-else:
+not_result = True
+pattern = re.compile(r'[qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM][AEIOUaeiou][AEIOUaeiou]+[qwrtypsdfghjklzxcvbnm'
+                     r'QWRTYPSDFGHJKLZXCVBNM]')
+match = re.search(pattern, source_s)
+while match:
+    # text_pos = match.span()
+    # print('match.end =', match.end())
+    find_str = re.findall(r'[AEIOUaeiou][AEIOUaeiou]+', source_s[match.start():match.end()])
+    if len(find_str[0]):
+        not_result = False
+        print(find_str[0])
+
+    source_s = source_s[match.end() - 1:]
+    # print(source_s[match.start():match.end()])
+    match = re.search(pattern, source_s)
+
+if not_result:
     print(-1)
 
 
