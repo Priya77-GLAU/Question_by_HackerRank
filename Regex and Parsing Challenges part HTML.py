6c09ec1eb6b918be14a3bf8cd6ac98f4243ce24d
YudinYury <yudin.yury@gmail.com>
2017-08-16 10:29:36 +0300

    Here I continue to solve the chellenges from the section Regex and Parsing

---
+++ Regex and Parsing Challenges part HTML.py
@@ -0,0 +1,20 @@
+''' Here I continue to solve the chellenges from the section Regex and Parsing
+TODO 13: "HTML Parser - Part 1"
+
+
+'''
+
+
+# TODO 13: "HTML Parser - Part 1"
+def html_parser_part_1():
+    pass
+
+    return 0
+
+
+def main():
+    html_parser_part_1()
+
+
+if __name__ == '__main__':
+    main()
