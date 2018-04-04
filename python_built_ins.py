1c6c81ff4327802754372ad7ade2b398a1d6d452
YudinYury <yudin.yury@gmail.com>
2018-04-04 11:46:17 +0300

    HackerRank task "Any or All" has been solved (just two lines of code)

--- python_built_ins.py
+++ python_built_ins.py
@@ -9,29 +9,27 @@ from operator import attrgetter
 # eval(input().strip())
 
 # TODO 2 "Athlete Sort"
-def by_second_item(item):
-    return item[1]
+# def by_second_item(item):
+#     return item[1]
+#
+# k = 0
+# def by_k_item(item):
+#     return item[k]
+#
+# def print_arr(arr):
+#     for item in arr:
+#         print(' '.join(map(str, item)))
+#
+# n, m = input().strip().split(' ')
+# n, m = [int(n), int(m)]
+# arr = []
+# for arr_i in range(n):
+#     arr_1 = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+#     arr.append(arr_1)
+# k = int(input().strip())
+# sorted_arr = sorted(arr, key=by_k_item)
+# print_arr(sorted_arr)
 
-
-k = 0
-
-
-def by_k_item(item):
-    return item[k]
-
-
-def print_arr(arr):
-    for item in arr:
-        print(' '.join(map(str, item)))
-
-
-n, m = input().strip().split(' ')
-n, m = [int(n), int(m)]
-arr = []
-for arr_i in range(n):
-    arr_1 = [int(arr_temp) for arr_temp in input().strip().split(' ')]
-    arr.append(arr_1)
-k = int(input().strip())
-
-sorted_arr = sorted(arr, key=by_k_item)
-print_arr(sorted_arr)
+# TODO 3 "Any or All"
+numbers = input().strip().split(' ') if int(input().strip()) else None
+print(all(list(map(lambda x: int(x) > 0, numbers))) and any(list(map(lambda x: str(x) == str(x)[::-1], numbers))))
